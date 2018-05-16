8c0df220bdef14e0aa89ee44237ad7e8f190f878
Karson <18055855754@163.com>
2018-05-16 17:11:10 +0800

    Initial Commit

---
+++ BaseFictitiousCoin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseFictitiousCoin
+//
+//  Created by Adxyun_Karson on 2018/5/16.
+//  Copyright © 2018年 AiQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseFictitiousCoin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseFictitiousCoin
+//
+//  Created by Adxyun_Karson on 2018/5/16.
+//  Copyright © 2018年 AiQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
